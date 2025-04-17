2d8b5939c57e41a76e3c903bd4ae2d7b4173021e
miiv0 <minzana2002@gmail.com>
2025-04-16 20:16:34 -0400

    made some style in css for the site and added some buttons, made the grid align

--- javascript.js
+++ javascript.js
@@ -3,10 +3,18 @@ const container = document.querySelector(".container");
 
 
 function createGrid(row, column) {
+    container.innerHTML = '';
+
+    const rowSize = 100 / row;
+
+    const columnSize = 100 / column;
+    console.log(rowSize);
+    console.log(columnSize);
+
     for (let i = 0; i < row * column; i++) {
         const square = document.createElement("div");
-        square.style.width = "50px";
-        square.style.height = "50px";
+        square.style.width = `${rowSize}%`;
+        square.style.height = `${columnSize}%`;
         square.classList.add("square");
         container.appendChild(square);
 
